0a12b36c071eb3816c9209b9f9bbd93d13fd2ca1
pavelhardzei <pavelgordei11@gmail.com>
2021-06-23 17:58:14 +0300

    registered post model

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'Tom',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'June 23, 2021'
-    },
-    {
-        'author': 'Bob',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'June 23, 2021'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context=context)
