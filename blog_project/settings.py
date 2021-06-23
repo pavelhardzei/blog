fda4ef2b5c2f165f742f74b734eb937bec25827a
pavelhardzei <pavelgordei11@gmail.com>
2021-06-23 19:06:55 +0300

    installed crispy_forms

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'users'
+    'users',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
