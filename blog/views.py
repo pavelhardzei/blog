367ab39b0fe89759fe4f0fb5752b2d3e11190d5e
pavelhardzei <pavelgordei11@gmail.com>
2021-06-23 11:51:10 +0300

    added test posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Tom',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 23, 2021'
+    },
+    {
+        'author': 'Bob',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 23, 2021'
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
