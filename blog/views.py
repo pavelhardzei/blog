a02a0593617574c2b0d22a6a59738f1830aecec0
pavelhardzei <pavelgordei11@gmail.com>
2021-06-23 19:59:25 +0300

    added user restriction

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
+
+@login_required
 def home(request):
     return render(request, 'blog/home.html', {'posts': Post.objects.all(), 'title': 'Home'})
 
+
+@login_required
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -135,4 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_REDIRECT_URL = 'profile'
+LOGIN_URL = 'login'
